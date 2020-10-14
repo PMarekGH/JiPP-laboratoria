efa285b8b5775329622dbce24e5c6116654d96e2
Przemysław Marek <przemyslawmarek516@gmail.com>
2020-10-14 17:36:07 +0200

    Added zad.2

---
+++ 13.10.2020/zad.2/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+bool jestPalindromem(std::string napis);
+
+int main(int argc, char *argv[])
+{
+    std::string napis;
+    int length;
+
+    if(argc == 1)
+    {
+        std::cout << "Nie podano argumentu!" << std::endl;
+        return 0;
+    }
+
+    if(jestPalindromem(argv[1]))
+    {
+        std::cout << "Napis jest palindromem";
+    }
+    else
+    {
+        std::cout << "Napis nie jest palindromem";
+    }
+    
+
+    return 0;
+}
+
+bool jestPalindromem(std::string napis)
+{
+    if(napis.length() % 2 == 0)
+        {
+            for(int i = 0; i < napis.length() / 2; i++)
+            {
+                if(napis[i] != napis[napis.length() - i - 1])
+                {
+                    return 0; // Zwraca 0 gdy napis jest palindromem
+                }
+                return 1;     // Zwraca 1 gdy napis jest palindromem
+            }
+        }
+    else
+        {
+            for(int i = 0; i < napis.length() / 2; i++)
+            {
+                if(napis[i] != napis[napis.length() - i - 1])
+                {
+                    return 0; 
+                }
+                return 1;
+            }
+        }
+}
